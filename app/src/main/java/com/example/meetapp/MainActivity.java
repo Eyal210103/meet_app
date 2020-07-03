0994da214bca3a8e55751a5ecf44b1069d9f02ea
Eyal Darmon <64871493+Eyal210103@users.noreply.github.com>
2020-07-03 19:25:10 +0300

    INIT

--- app/src/main/java/com/example/meetapp/MainActivity.java
+++ app/src/main/java/com/example/meetapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.meetapp;
 
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
+import com.example.meetapp.model.CurrentUser;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 //        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 //        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 //        NavigationUI.setupWithNavController(navView, navController);
+
     }
 
 }
