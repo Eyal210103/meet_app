bb2d012bbb320657947cb14ec60bd01d60e86e32
Eyal Darmon <64871493+Eyal210103@users.noreply.github.com>
2020-07-03 23:14:22 +0300

    bottom nav with nav comp

--- app/src/main/java/com/example/meetapp/MainActivity.java
+++ app/src/main/java/com/example/meetapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.bumptech.glide.Glide;
 import com.example.meetapp.model.CurrentUser;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -16,20 +17,18 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-//                .build();
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-//        NavigationUI.setupWithNavController(navView, navController);
 
+        BottomNavigationView navView = findViewById(R.id.bottom_nav);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder().build();
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(navView,navController);
     }
+
 
 }
---
+++ app/src/main/java/com/example/meetapp/groupInfo/GroupInfoViewModel.java
@@ -0,0 +1,7 @@
+package com.example.meetapp.groupInfo;
+
+import androidx.lifecycle.ViewModel;
+
+public class GroupInfoViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
