b586edc4bbb30c6badc457104358915decebe002
Eyal Darmon <64871493+Eyal210103@users.noreply.github.com>
2020-07-04 14:17:22 +0300

    bottom nav with nav comp

--- app/src/main/java/com/example/meetapp/firebaseActions/DatabaseWrite.java
+++ app/src/main/java/com/example/meetapp/firebaseActions/DatabaseWrite.java
@@ -1,4 +1,4 @@
-package com.example.meetapp;
+package com.example.meetapp.firebaseActions;
 
 import androidx.annotation.NonNull;
 
---
+++ app/src/main/java/com/example/meetapp/ui/socialInfo/SocialMenuViewModel.java
@@ -0,0 +1,7 @@
+package com.example.meetapp.ui.socialInfo;
+
+import androidx.lifecycle.ViewModel;
+
+public class SocialMenuViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
