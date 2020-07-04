6bb07cea64aa9e146b83c107bf53fe7b0240607f
Eyal Darmon <64871493+Eyal210103@users.noreply.github.com>
2020-07-04 14:19:49 +0300

    bottom nav with nav comp + add Fragments

---
+++ app/src/main/java/com/example/meetapp/ui/settings/settingsFragment.java
@@ -0,0 +1,38 @@
+package com.example.meetapp.ui.settings;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.meetapp.R;
+
+public class settingsFragment extends Fragment {
+
+    private SettingsViewModel mViewModel;
+
+    public static settingsFragment newInstance() {
+        return new settingsFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.settings_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(SettingsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
