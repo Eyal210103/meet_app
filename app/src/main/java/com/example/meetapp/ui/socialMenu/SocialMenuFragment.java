3dd4ca868ce4614b5dde933fb24b384a8d87616c
Eyal Darmon <64871493+Eyal210103@users.noreply.github.com>
2020-07-04 21:47:18 +0300

    bottom nav with nav comp + add Fragments

--- app/src/main/java/com/example/meetapp/ui/socialMenu/SocialMenuFragment.java
+++ app/src/main/java/com/example/meetapp/ui/socialMenu/SocialMenuFragment.java
@@ -1,5 +1,9 @@
 package com.example.meetapp.ui.socialMenu;
 
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Context;
@@ -8,8 +12,10 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.util.AttributeSet;
@@ -18,8 +24,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.meetapp.R;
+import com.example.meetapp.ui.home.HomeFragment;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.Objects;
 
 public class SocialMenuFragment extends Fragment {
 
@@ -29,20 +38,18 @@ public class SocialMenuFragment extends Fragment {
         return new SocialMenuFragment();
     }
 
+    private String[] titles = new String[]{"Chat", "Groups", "Meetings"};
+    ViewPager2 viewPager;
+    TabLayout tabLayout;
+
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.social_menu_fragment, container, false);
-        TabLayout tabLayout = view.findViewById(R.id.tab_layout);
-        ViewPager2 viewPager = view.findViewById(R.id.viewpager);
-        //viewPager.setAdapter(new );
-        new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
-            @Override
-            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
-                tab.setText("OBJECT " + (position + 1));
-            }
-        }
-        ).attach();
+        tabLayout = view.findViewById(R.id.tab_layout);
+        viewPager = view.findViewById(R.id.viewpager);
+        init();
         return view;
     }
 
@@ -52,16 +59,46 @@ public class SocialMenuFragment extends Fragment {
         mViewModel = ViewModelProviders.of(this).get(SocialMenuViewModel.class);
     }
 
-}
-class ViewPagerAdapter extends PagerAdapter {
 
+    private void init() {
+
+        viewPager.setAdapter(new ViewPagerFragmentAdapter(getActivity()));
+        new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText("OBJECT " + (position + 1));
+            }
+        }
+        ).attach();
+    }
+
+
+
+
+}
+class ViewPagerFragmentAdapter extends FragmentStateAdapter {
+
+    public ViewPagerFragmentAdapter(FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
     @Override
-    public int getCount() {
-        return 0;
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new HomeFragment();
+            case 1:
+                return new HomeFragment();
+            case 2:
+                return new HomeFragment();
+        }
+        return new HomeFragment();
     }
 
     @Override
-    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-        return false;
+    public int getItemCount() {
+        return 3;
     }
 }
+
