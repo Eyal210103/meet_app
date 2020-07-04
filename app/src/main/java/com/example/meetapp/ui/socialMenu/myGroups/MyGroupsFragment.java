adaffbbf624359951e0ed3d4d273dd1b3d0adbec
Eyal Darmon <64871493+Eyal210103@users.noreply.github.com>
2020-07-04 21:58:51 +0300

    Social Fragment

---
+++ app/src/main/java/com/example/meetapp/ui/socialMenu/myGroups/MyGroupsFragment.java
@@ -0,0 +1,38 @@
+package com.example.meetapp.ui.socialMenu.myGroups;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.meetapp.R;
+
+public class MyGroupsFragment extends Fragment {
+
+    private MyGroupsViewModel mViewModel;
+
+    public static MyGroupsFragment newInstance() {
+        return new MyGroupsFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.my_groups_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MyGroupsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/meetapp/ui/socialMenu/myGroups/MyGroupsViewModel.java
@@ -0,0 +1,7 @@
+package com.example.meetapp.ui.socialMenu.myGroups;
+
+import androidx.lifecycle.ViewModel;
+
+public class MyGroupsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
