7ceea484b9da56005f0237b17491155d822e5009
Eyal Darmon <64871493+Eyal210103@users.noreply.github.com>
2020-07-05 12:17:58 +0300

    add Groups and Meeting Classes

---
+++ app/src/main/java/com/example/meetapp/ui/groupInfo/GroupInfoFragment.java
@@ -0,0 +1,38 @@
+package com.example.meetapp.ui.groupInfo;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.meetapp.R;
+
+public class GroupInfoFragment extends Fragment {
+
+    private GroupInfoViewModel mViewModel;
+
+    public static GroupInfoFragment newInstance() {
+        return new GroupInfoFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.group_info_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(GroupInfoViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
