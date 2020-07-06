fcd77e9e7b5e6d60e317d642f7c8ee2f4b28c373
Eyal Darmon <64871493+Eyal210103@users.noreply.github.com>
2020-07-06 18:21:22 +0300

    ViewModel My Groups

--- app/src/main/java/com/example/meetapp/ui/socialMenu/myGroups/MyGroupsViewModel.java
+++ app/src/main/java/com/example/meetapp/ui/socialMenu/myGroups/MyGroupsViewModel.java
@@ -1,7 +1,27 @@
 package com.example.meetapp.ui.socialMenu.myGroups;
 
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.meetapp.firebaseActions.UserGroupsRepo;
+import com.example.meetapp.model.Group;
+
+import java.util.Map;
+
 public class MyGroupsViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    MutableLiveData<Map<String, MutableLiveData<Group>>> map = null;
+
+    public void init(@NonNull Context context) {
+        if (map == null){
+            map = UserGroupsRepo.getInstance(context).getGroups();
+        }
+    }
+
+    public LiveData<Map<String, MutableLiveData<Group>>> getGroupMutableLiveData() {
+        return map;
+    }
 }
