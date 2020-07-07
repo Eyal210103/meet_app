4cc9be4d0b5796cb1bedaf9131cd8bba78cf4f56
Eyal Darmon <64871493+Eyal210103@users.noreply.github.com>
2020-07-07 16:37:24 +0300

    Create Group Layout + navigation

--- app/src/main/java/com/example/meetapp/firebaseActions/UserGroupsRepo.java
+++ app/src/main/java/com/example/meetapp/firebaseActions/UserGroupsRepo.java
@@ -13,14 +13,11 @@ import com.example.meetapp.model.Group;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 public class UserGroupsRepo {
 
