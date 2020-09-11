fd94d16f8f98088bb87167d1f06ff6a2532140a3
Eyal Darmon <64871493+Eyal210103@users.noreply.github.com>
2020-09-11 17:26:34 +0300

    update chat color

---
+++ app/src/main/java/com/example/meetapp/ui/socialMenu/myGroups/JoinGroupFragment.java
@@ -0,0 +1,35 @@
+package com.example.meetapp.ui.socialMenu.myGroups;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.meetapp.R;
+
+public class JoinGroupFragment extends Fragment {
+
+    private JoinGroupViewModel mViewModel;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.join_group_fragment, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(JoinGroupViewModel.class);
+    }
+
+}
