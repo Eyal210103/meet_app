4bb7d2eb55f6b42e11eb049d4c43d7b397104399
Eyal Darmon <64871493+Eyal210103@users.noreply.github.com>
2020-09-20 17:12:01 +0300

    map on meetings

--- app/src/main/java/com/example/meetapp/ui/groupInfo/groupsMeetings/GroupMeetingsFragment.java
+++ app/src/main/java/com/example/meetapp/ui/groupInfo/groupsMeetings/GroupMeetingsFragment.java
@@ -1,31 +1,69 @@
 package com.example.meetapp.ui.groupInfo.groupsMeetings;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.meetapp.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
 public class GroupMeetingsFragment extends Fragment {
 
     private GroupMeetingsViewModel mViewModel;
 
+    private MapView mapView;
+    private GoogleMap mMap;
+    private double latitude = 0;
+    private double longitude = 0;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.group_meetings_fragment, container, false);
+        View view = inflater.inflate(R.layout.group_meetings_fragment, container, false);
+        this.mapView = view.findViewById(R.id.group_meetings_mapView);
+
+        initGoogleMap(savedInstanceState);
+
+        return view;
+    }
+
+    private void initGoogleMap(Bundle savedInstanceState) {
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
+                if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    googleMap.setMyLocationEnabled(true);
+                    googleMap.setBuildingsEnabled(false);
+                    googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+//                    mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(latitude,longitude)));
+                    mMap.moveCamera(CameraUpdateFactory.zoomTo(10f));
+                    //seekBar.setProgress((int) (mMap.getCameraPosition().zoom*5));
+                } else {
+                    Toast.makeText(getActivity(), "Map Error", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     @Override
@@ -34,4 +72,39 @@ public class GroupMeetingsFragment extends Fragment {
         mViewModel = ViewModelProviders.of(this).get(GroupMeetingsViewModel.class);
     }
 
+    @Override
+    public void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
 }
