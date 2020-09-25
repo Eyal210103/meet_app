12cc25696514e621504b54b4b33367916d1eaae4
Eyal Darmon <64871493+Eyal210103@users.noreply.github.com>
2020-09-26 00:04:32 +0300

    sign up fragment  - design + create user(with StorageUpload) - NOT COMPLETED

--- app/src/main/java/com/example/meetapp/firebaseActions/StorageUpload.java
+++ app/src/main/java/com/example/meetapp/firebaseActions/StorageUpload.java
@@ -10,7 +10,12 @@ import androidx.fragment.app.Fragment;
 import com.example.meetapp.uploadsListeners.PhotoUploadCompleteListener;
 import com.example.meetapp.uploadsListeners.PhotoUploadErrorListener;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -34,7 +39,7 @@ public class StorageUpload {
                                         PhotoUploadCompleteListener photoUploadCompleteListener = (PhotoUploadCompleteListener)context;
                                         photoUploadCompleteListener.onPhotoUploadComplete();
                                     }else {
-                                        reference.child("Group").child(groupId).delete();
+                                        reference.child("Groups Images").child(groupId).delete();
                                         PhotoUploadErrorListener photoUploadErrorListener = (PhotoUploadErrorListener)context;
                                         photoUploadErrorListener.onPhotoUploadError();
                                     }
@@ -43,5 +48,48 @@ public class StorageUpload {
                         }
                     }
                 });
+    }
+
+    public static void uploadProfileImage(final Fragment context, final String id , Uri data){
+        reference.child("Profile Images").child(id).putFile(data).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if (task.isSuccessful()){
+                    reference.child("Profile Images").child(id).getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            if (task.isSuccessful()){
+                                database.getReference().child("Users").child(id).child("profileImageUrl").setValue(task.getResult().toString());
+                                UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder()
+                                        .setPhotoUri(Uri.parse(task.getResult().toString()))
+                                        .build();
+                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                if (user != null) {
+                                    user.updateProfile(profileUpdate).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            PhotoUploadCompleteListener photoUploadCompleteListener = (PhotoUploadCompleteListener)context;
+                                            photoUploadCompleteListener.onPhotoUploadComplete();
+                                        }
+                                    }).addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            reference.child("Profile Images").child(id).delete();
+                                            PhotoUploadErrorListener photoUploadErrorListener = (PhotoUploadErrorListener)context;
+                                            photoUploadErrorListener.onPhotoUploadError();
+                                        }
+                                    });
+                                }
+
+                            }else {
+                                reference.child("Profile Images").child(id).delete();
+                                PhotoUploadErrorListener photoUploadErrorListener = (PhotoUploadErrorListener)context;
+                                photoUploadErrorListener.onPhotoUploadError();
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/meetapp/ui/Login/SignupFragment.java
+++ app/src/main/java/com/example/meetapp/ui/Login/SignupFragment.java
@@ -1,7 +1,9 @@
 package com.example.meetapp.ui.Login;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,10 +11,25 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.meetapp.R;
+import com.example.meetapp.firebaseActions.DatabaseWrite;
+import com.example.meetapp.firebaseActions.StorageUpload;
+import com.example.meetapp.model.CurrentUser;
+import com.example.meetapp.uploadsListeners.PhotoUploadCompleteListener;
+import com.example.meetapp.uploadsListeners.PhotoUploadErrorListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
-public class SignupFragment extends Fragment {
+public class SignupFragment extends Fragment implements PhotoUploadErrorListener, PhotoUploadCompleteListener {
 
     View view;
+
+    //https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,6 +41,73 @@ public class SignupFragment extends Fragment {
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_signup, container, false);
 
+
         return view;
+    }
+
+    private void signUpWithEmail(String display , String email, String password){
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(SignupFragment.this.requireActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user =  FirebaseAuth.getInstance().getCurrentUser();
+                            UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(display)
+                                    .setPhotoUri(Uri.parse("https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"))
+                                    .build();
+                            if (user != null) {
+                                user.updateProfile(profileUpdate).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        CurrentUser.firebaseUserToAppUser(user);
+                                        DatabaseWrite.addOrUpdateUser(CurrentUser.getCurrentUser());
+                                        //TODO implement select Image
+                                    }
+                                });
+                            }
+                        } else {
+                        }
+
+                    }
+                });
+    }
+
+    private boolean validateSignUp(String display , String email, String pass , String passConfirm){
+        if (display.matches("")){
+            Snackbar snackbar = Snackbar
+                    .make(view, "Please Choose A Display Name", Snackbar.LENGTH_LONG);
+            snackbar.show();
+            return false;
+        }
+        else if (email.matches("") || email.indexOf('@') == -1 || email.indexOf('.') == -1 || email.charAt(0) == '@' ){
+            Snackbar snackbar = Snackbar
+                    .make(view, "Invalid Email Address", Snackbar.LENGTH_LONG);
+            snackbar.show();
+            return false;
+        }
+        else if (pass.length() < 6){
+            Snackbar snackbar = Snackbar
+                    .make(view, "A Password Needs To Contained At Least 6 Characters", Snackbar.LENGTH_LONG);
+            snackbar.show();
+            return false;
+        }
+        else if (!pass.matches(passConfirm)){
+            Snackbar snackbar = Snackbar
+                    .make(view, "Passwords Are Not Identical", Snackbar.LENGTH_LONG);
+            snackbar.show();
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onPhotoUploadError() {
+
+    }
+
+    @Override
+    public void onPhotoUploadComplete() {
+
     }
 }
