1ee8ad04dbb614ede198d8a08b96f29f72ab4982
Eyal Darmon <64871493+Eyal210103@users.noreply.github.com>
2020-10-10 14:54:52 +0300

    CreateMeetingFragment.java

---
+++ app/src/main/java/com/example/meetapp/ui/CreateMeetingFragment.java
@@ -0,0 +1,25 @@
+package com.example.meetapp.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.meetapp.R;
+
+public class CreateMeetingFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.fragment_create_meeting, container, false);
+        return view;
+    }
+}
