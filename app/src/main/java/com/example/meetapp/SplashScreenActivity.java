dd7a4353613d7cb51dfaa29e3c65a9a9684f60a4
Eyal Darmon <64871493+Eyal210103@users.noreply.github.com>
2020-12-18 23:29:17 +0200

    animations + SplashScreenActivity

---
+++ app/src/main/java/com/example/meetapp/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.example.meetapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.meetapp.ui.Login.LoginActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+    }
+}
---
+++ app/src/main/java/com/example/meetapp/ui/meetings/MeetingsInfoDialog.java
@@ -0,0 +1,28 @@
+package com.example.meetapp.ui.meetings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.meetapp.R;
+
+public class MeetingsInfoDialog extends DialogFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view =  inflater.inflate(R.layout.meeting_info_dialog, container, false);
+        return view;
+    }
+}
