2a69578006a112355c71b7f11901a070b94659c2
eyal darmon <eyal210103eyal@gmail.com>
2021-04-25 09:30:41 +0300

    notifications for meetings

--- app/src/main/java/com/example/meetapp/firebaseActions/FirebaseTags.java
+++ app/src/main/java/com/example/meetapp/firebaseActions/FirebaseTags.java
@@ -1,7 +1,6 @@
 package com.example.meetapp.firebaseActions;
 
 public class FirebaseTags {
-
     //Database Related
     public static final String GROUPS_CHILDES = "Groups";
     public static final String GROUP_TYPE_CHILDES = "Group";
@@ -11,14 +10,11 @@ public class FirebaseTags {
     public static final String MANAGER_CHILDES = "Waiting";
     public static final String MEMBERS_CHILDES = "Members";
     public static final String MEETINGS_CHILDES = "Meetings";
-
     public static final String PUBLIC_MEETINGS_CHILDES = "Meetings/Public";
     public static final String GROUP_MEETINGS_CHILDES = "Meetings/Group";
     public static final String WHO_COMING_CHILDES = "whoComing";
-
     //Query Related
     public static final String SORT_GROUPS_NAME = "name";
-
     //Storage Related
     public static final String STORAGE_GROUP_IMAGE = "Groups Images";
     public static final String GROUPS_PHOTO_URL_CHILD = "photoUrl";
@@ -28,7 +24,4 @@ public class FirebaseTags {
     public static final String STORAGE_CHAT_IMAGES = "Chat Images";
     public static final String STORAGE_PROFILE_IMAGES = "Profile Images";
     public static final String USER_PHOTO_URL_CHILD = "profileImageUrl";
-
-
-
 }
