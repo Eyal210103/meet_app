319545dc001fea1d72347ed9f542aac4781d39e1
eyal darmon <eyal210103eyal@gmail.com>
2021-06-27 01:50:09 +0300

    bugs

--- app/src/main/java/com/example/meetapp/chatPushNotification/CreateMeetingService.java
+++ app/src/main/java/com/example/meetapp/chatPushNotification/CreateMeetingService.java
@@ -6,6 +6,7 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.media.RingtoneManager;
@@ -56,33 +57,35 @@ public class CreateMeetingService extends Service {
     }
 
     private void sendOreoNotification(long date){
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+        channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        ((NotificationManager) this.getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE)).createNotificationChannel(channel);
+
         NotificationCompat.Builder builder = getOreoNotification(date);
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
         notificationManager.notify(12, builder.build());
     }
     private NotificationCompat.Builder getOreoNotification(long meetingDate) {
-        NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
-                CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
-        channel.enableLights(false);
-        channel.enableVibration(true);
-        channel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
-
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date(meetingDate));
 
         String title = "Congrats";
         @SuppressLint("DefaultLocale")
-        String body = "Your Meeting at ";//"+ String.format("%d,%d %2d:%2d",calendar.get(Calendar.DAY_OF_MONTH),calendar.get(Calendar.MONTH),calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE)) +" Has Successfully Created";
+        String body = "Your Meeting at "+ String.format("%d/%d on %02d:%02d",calendar.get(Calendar.DAY_OF_MONTH),calendar.get(Calendar.MONTH),calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE)) +" Has Successfully Created";
 
         Intent intent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
 
-        return new NotificationCompat.Builder(this, CHANNEL_NAME)
-                .setSmallIcon(R.mipmap.ic_launcher_round)
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_NAME)
+                .setSmallIcon(R.drawable.ic_notifications_black_24dp)
                 .setContentTitle(title)
-                .setContentText(body)
-                .setContentIntent(pendingIntent)
                 .setChannelId(CHANNEL_ID)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true)
+                .setContentText(body);
+
+
+        return builder;
     }
 }
